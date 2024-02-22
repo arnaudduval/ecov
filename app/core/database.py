eec22587b1d6fb43310769c22209b4b796f8db38
Arnaud Duval <arnaud.duval@gmail.com>
2024-02-22 20:30:28 +0100

    First commit: display a static page

---
+++ app/core/database.py
@@ -0,0 +1,23 @@
+"""
+Database management
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from app.core.config import settings
+
+engine = create_engine(
+    settings.SQLITE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/main.py
@@ -0,0 +1,27 @@
+"""
+Application entry point
+"""
+
+from fastapi import Depends, FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+from app.core.config import settings, templates
+from app.core.database import Base, engine
+# from app.views.member import members_views
+
+app=FastAPI()
+
+app.mount("/public", StaticFiles(directory=settings.STATIC_FILES_DIR), name="static")
+
+Base.metadata.create_all(bind=engine)
+
+# app.include_router(member_views, tags=["Members"])
+
+#@app.get("/", include_in_schema=False)
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html"
+    )
